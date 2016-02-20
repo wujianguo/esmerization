37b35ac6b11bd4ef6dd40c2fba44814239354eb7
wujianguo <lsjustin89@gmail.com>
2016-02-20 15:10:19 +0800

    download vlc shell

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -4,5 +4,5 @@ cd `dirname $0`
 wget http://nightlies.videolan.org/build/ios/MobileVLCKit-UniversalBinary-20160219-1708.zip -O mobilevlckit.zip
 
 unzip mobilevlckit.zip
-mv MobileVLCKit-binary/MobileVLCKit.framework ./
+mv build/MobileVLCKit.framework ./
 
